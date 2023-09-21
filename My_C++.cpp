2275784043509d827e605f05e19610fec87580a8
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-09-21 17:55:45 +0530

    Update My_C++.cpp

--- My_C++.cpp
+++ My_C++.cpp
@@ -13,7 +13,7 @@ comment */
 int main()
 {
   cout<< "its we v here"; 
-  int num ;//defining a variable with use case in 
+  int num ;//defining a variable with use case in  integer
   num = 6;//assigning a value to the variable
   cout<< num ;
   int Age = 26;
