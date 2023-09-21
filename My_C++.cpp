62dcab094db0aed47382197d22b278fff6ebbfd7
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-09-21 17:55:10 +0530

    Update My_C++.cpp

--- My_C++.cpp
+++ My_C++.cpp
@@ -13,7 +13,7 @@ comment */
 int main()
 {
   cout<< "its we v here"; 
-  int num ;//defining a variable 
+  int num ;//defining a variable with
   num = 6;//assigning a value to the variable
   cout<< num ;
   int Age = 26;
