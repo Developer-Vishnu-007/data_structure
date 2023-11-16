988394814af3017b637ffc205a329d3009f12c9d
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-11-16 22:59:27 +0530

    Update array.cpp

--- Contents/array.cpp
+++ Contents/array.cpp
@@ -7,7 +7,7 @@ int main(){
     myArray[0]=10;
     myArray[1]=20;
     myArray[2]=30;
-    myArray[3] = 40;
+    myArray[3]= 40;
     myArray[4] = 50;
 
     // Accessing and printing values from the array
