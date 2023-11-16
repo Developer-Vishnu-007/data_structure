68c98614cfa5465cd6b1f29946f8b28edbd2a430
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-11-16 22:58:39 +0530

    Update array.cpp

--- Contents/array.cpp
+++ Contents/array.cpp
@@ -4,7 +4,7 @@ int main(){
     // Creating an array of integers with a specified size (in this case, 5)
     int myArray[5]; 
     // Assigning values to the elements of the array
-    myArray[0] = 10;
+    myArray[0]= 10;
     myArray[1] = 20;
     myArray[2] = 30;
     myArray[3] = 40;
