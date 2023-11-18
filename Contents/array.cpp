4631928d5da06644da2568a65a6df402b8b0a056
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-11-18 23:40:58 +0530

    Update array.cpp

--- Contents/array.cpp
+++ Contents/array.cpp
@@ -11,7 +11,7 @@ int main(){
     myArray[4]=50;
 // Accessing and printing values from the array
     std::cout<<"Elements of the array:";
-    for(int i=0; i< 5; ++i){
+    for(int i=0; i<5; ++i){
         std::cout<<myArray[i]<<"";
     }
     
